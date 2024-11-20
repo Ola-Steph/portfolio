3403b2ad9936a795e0444fe9269e2cf53202a6e3
Macbook <stephjaiye@gmail.com>
2024-11-20 11:53:06 +0100

    fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import HeroSection from '@/components/HeroSection';
 import AboutSection from '@/components/AboutSection';
 import ProjectsSection from '@/components/ProjectsSection';
 import ContactSection from '@/components/ContactSection';
-import Footer from '@/components/footer';
+import Footer from '@/components/Footer';
 
 export default function Home() {
   return (
